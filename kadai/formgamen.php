8349e2572581727a4b2c583df2752ea04dafb4b5
231057 <ikuyoshi7300@gmail.com>
2024-06-25 14:36:41 +0900

    まだ変えてない

--- kadai/formgamen.php
+++ kadai/formgamen.php
@@ -6,9 +6,10 @@
 </head>
 <body>
     <h1>　　　　あなたの運勢おみくじタッチで占いましょう！！ </h1>
-    <table border="2">
+    <table border="2" width="100" height="150" cellpadding="300">
         <tr>
-            <th>スタート？？</th>
+            <th>スタート</th>
+            <th bgcolor="green">年齢</th>
             
             
         </tr>
