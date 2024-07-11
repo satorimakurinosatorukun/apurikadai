a5d768b869009d7ff05f3f7cf2e5bee0bad1a18b
231024 <0108soua@gmail.com>
2024-07-11 22:23:08 +0900

    内容の表示完了

--- kadai/page3.php
+++ kadai/page3.php
@@ -1,43 +1,37 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "omikuji";
-$dsn = "mysql:host={$host};dbname={$dbname};charset=utf8";
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $dbname = "omikuji";
+    $dsn = "mysql:host={$host};dbname={$dbname};charset=utf8";
 
-// HTMLエスケープ用の関数
-function es($data) {
-    
-    return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
-}
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // form method = postからデータを受け取る
-    if(isset($_POST['touroku_b'])){//登録ボタンが押された場合
-        echo 'a';
-        try {
-            echo 'b';
-            $sql_select = "SELECT un FROM `unki`WHERE ID IN (1, 2, 3, 4)   ORDER BY RAND() LIMIT 1 ";
-            $stm_select = $pdo->prepare($sql_update);
-            $stm_update->execute();
-            echo $sql_select;
-        } catch(Exception $e) {
-            echo '<span>エラー</span><br>';
-            echo $e->getMessage();
-            exit();
-        }
+    // HTMLエスケープ用の関数
+    function es($data) {
+        return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
     }
     
-}
-echo "<tbody>";
-#es関数の処理は上のfunction esに記述している
-foreach ($result as $row) {
-    echo "<tr>";
-    echo "<td>", es($row['']), "</td>";
-    echo "<td>", es($row['']), "</td>";
-    echo "</tr>";
-}
-echo "</tbody>";
 
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // form method = postからデータを受け取る
+        if(isset($_POST['touroku_b'])){//登録ボタンが押された場合
+            try {
+                // データベースへの接続
+                $pdo = new PDO($dsn, $user, $password);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $sql_select = "SELECT un FROM `unki`WHERE ID IN (1, 2, 3, 4)   ORDER BY RAND() LIMIT 1 ";
+                $stm_select = $pdo->prepare($sql_select);
+                $stm_select->execute();
+                $row = $stm_select->fetch(PDO::FETCH_ASSOC); // 結果を取得
+
+            } catch(Exception $e) {
+                echo '<span>エラー</span><br>';
+                echo $e->getMessage();
+                exit();
+            }
+        }
+    }
 ?>  
 <!DOCTYPE html>
 <html lang="ja">
@@ -60,19 +54,30 @@ echo "</tbody>";
                 line-height:10px;
             }
         </style>
-        <table>
-            <tr><!-- 3つのth -->
-                <th>運勢</th> <th>内容</th> 
-            </tr>
-            <tr><!-- 3つのtd -->
-                <td></td> <td></td> 
-            </tr>
-        </table>
-        
-        
-
         <form action="page3.php" method="post">
-            <button id="result" type="submit" name ="kekka_b" >結果を見る</button>
+            <table border="5">
+                <tr>
+                    <th>運勢</th> <th>内容</th> 
+                </tr>
+                <tr>
+                    <td>
+                        <div>
+                            <?php echo isset($row['un']) ? es($row['un']) : ''; ?>  
+                        </div>
+                    </td> 
+                    <td>
+
+                    </td> 
+                </tr>
+            </table>
+            <!-- <button id="result" type="submit" name ="kekka_b" >
+                結果を見る
+            </button> -->
+            <div class="touroku_button" id="Touroku">
+                <input name = "touroku_b" type="submit" value="結果を見る">
+            </div>
+
+
         </form>
         <!-- 戻るボタン -->
         <div id="back_button2">
